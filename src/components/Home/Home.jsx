9cb84f357ce536872c3ce8f2d739555acaffaa48
touat sliman mustapha <touat_mustapha@outlook.com>
2025-09-01 17:40:22 +0100

    refactor Home component: adjust layout structure, enhance spacing, and update social icons styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -31,21 +31,22 @@ function Home() {
       ></div>
 
       {/* Left Side Content */}
-      <div className="flex flex-col justify-start items-start text-slate-200 text-left pt-[5vh] md:pt-[20vh] px-6 md:pl-[5vw] md:h-[100vh] w-full lg:w-[30rem]  z-20">
-        <Type />
-        <h2 className="text-4xl md:text-[3.2rem] font-bold tracking-tight -tracking-[3px]">
-          Sliman Touat
-        </h2>
-        <h3 className="text-base mt-1 text-lg text-slate-300">
-          Full Stack Developer
-        </h3>
-        <p className="pt-4 max-w-xl text-sm leading-6 text-slate-400">
-          Building fast, scalable web apps with modern technologies.
-        </p>
-        <hr className="w-full my-4 border-slate-500" />
+      <div className="flex flex-col justify-between pb-10 items-start text-slate-200 text-left pt-[5vh] md:pt-[20vh] px-6 md:pl-[5vw] md:h-[100vh] w-full lg:w-[30rem]  z-20">
+        <div>
+          <Type />
+          <h2 className="text-4xl md:text-[3.2rem] font-bold tracking-tight -tracking-[3px]">
+            Sliman Touat
+          </h2>
+          <h3 className="text-base mt-1 text-lg text-slate-300">
+            Full Stack Developer
+          </h3>
+          <p className="pt-4 max-w-xl text-sm leading-6 text-slate-400">
+            Building fast, scalable web apps with modern technologies.
+          </p>
+        </div>
 
         {/* Social Icons */}
-        <div className="text-2xl flex gap-6 mt-4 mx-auto ">
+        <div className="text-3xl flex gap-6 mt-4 mx-auto ">
           <a href="#" aria-label="Facebook">
             <FaFacebook className="text-slate-400 hover:text-slate-300 transition duration-200" />
           </a>
@@ -74,29 +75,37 @@ function Home() {
       </div>
 
       {/* Button Group (at bottom on large, below content on small) */}
-      <div className="w-full lg:w-[30rem] lg:h-[80vh] px-6 lg:px-0 mt-10 mb-10 lg:mt-[5vh] lg:mb-0 flex  sm:flex-row justify-center sm:justify-evenly items-start gap-6 z-20">
-        <Link
-          to="/about"
-          className="w-12 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-center items-center hover:bg-slate-700 transition"
-        >
-          <LuBadgeHelp className="text-xl text-slate-300" />
-        </Link>
-        <Link
-          to="/projects"
-          className="w-12 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-center items-center hover:bg-slate-700 transition"
-        >
-          <AiOutlineFundProjectionScreen className="text-xl text-slate-300" />
-        </Link>
-        <Link
-          to="/resume"
-          className="px-4 no-underline py-2 w-auto sm:w-42 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-between items-center gap-4 hover:bg-slate-700 transition"
-        >
-          <span className="flex items-center text-slate-300">
-            <FaEye className="mr-1" />
-            <i className="text-xs">Resume</i>
-          </span>
-          <GoDownload className="text-slate-300 w-full" />
-        </Link>
+      <div className="w-full sm:h-92 lg:w-[30rem] text-slate-400 mt-10 text-sm flex flex-col justify-between items-center gap-6 mb-10 px-6 md:px-0 z-20">
+        <div className="w-full flex  sm:flex-row justify-center sm:justify-evenly items-center gap-6">
+          <Link
+            to="/about"
+            className="w-12 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-center items-center hover:bg-slate-700 transition"
+          >
+            <LuBadgeHelp className="text-xl text-slate-300" />
+          </Link>
+          <Link
+            to="/projects"
+            className="w-12 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-center items-center hover:bg-slate-700 transition"
+          >
+            <AiOutlineFundProjectionScreen className="text-xl text-slate-300" />
+          </Link>
+          <Link
+            to="/resume"
+            className="px-4 no-underline py-2 w-auto sm:w-42 h-12 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] backdrop-blur-sm rounded-full flex justify-between items-center gap-4 hover:bg-slate-700 transition"
+          >
+            <span className="flex items-center text-slate-300">
+              <FaEye className="mr-1" />
+              <i className="text-xs">Resume</i>
+            </span>
+            <GoDownload className="text-slate-300 w-full" />
+          </Link>
+        </div>
+        <p>
+          Loosely designed in <b>Figma</b> and coded in{" "}
+          <b>Visual Studio Code</b> by yours truly. Built with <b>React.js</b>{" "}
+          and <b>Tailwind CSS</b>, deployed with <b>Render</b>. All text is set
+          in the Inter typeface.
+        </p>
       </div>
     </section>
   );
